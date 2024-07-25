dfc62ee9394476abbc69c6e1560ea3ddeecebe0b
errantsky <code@errantsky.dev>
2024-07-25 11:28:01 -0700

    Enable os_mon for dev dashboard.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule PhoenixVault.MixProject do
   def application do
     [
       mod: {PhoenixVault.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
