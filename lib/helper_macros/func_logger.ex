53e9ea21445f375a6c4d93197adbbccb37f26523
errantsky <code@errantsky.dev>
2024-08-05 18:22:28 -0700

    Create func_logger.ex

---
+++ lib/helper_macros/func_logger.ex
@@ -0,0 +1,37 @@
+defmodule HelperMacros.FuncLogger do
+  require Logger
+
+  defmacro log_func({func, _, [args]} = call) do
+    
+    quote do
+      func_name = unquote(Atom.to_string(func))
+      
+      arg_vals =
+        Enum.map(unquote(args), fn
+          {name, val} = arg -> """
+          #{Atom.to_string(name)}:\n\t#{inspect(val, pretty: true)}
+          """
+          arg -> "#{inspect(arg, pretty: true)}"
+        end)
+        |> Enum.join("\n" <> String.duplicate("-", 20) <> "\n")
+
+      Logger.debug("""
+      log_func #{func_name} #{func_name} with the following params:\n
+      #{arg_vals}
+      #{String.duplicate("-", 20)}
+      """)
+
+      start_time = :os.system_time(:millisecond)
+      result = unquote(call)
+      end_time = :os.system_time(:millisecond)
+
+      Logger.debug("log_func #{func_name} Execution time was: #{(end_time - start_time) / 1000} ms")
+      Logger.debug("""
+        log_func #{func_name} The result was:\n#{inspect(result, pretty: true)}
+        """)
+      Logger.debug("log_func #{func_name} finished.")
+
+      result
+    end
+  end
+end
