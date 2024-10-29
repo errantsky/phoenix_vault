48bdc2ab2ac75576b46a71e924ebdf9536d9ad23
errantsky <code@errantsky.dev>
2024-10-29 07:31:38 -0700

    Update deps and Elixir version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixVault.MixProject do
     [
       app: :phoenix_vault,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -33,7 +33,7 @@ defmodule PhoenixVault.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.12"},
+      {:phoenix, "~> 1.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
